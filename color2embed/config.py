03899636f70ac63086ff36848c7e3c028ec3ab5c
Aleksey Alekseev <a.alekseev@ntechlab.com>
2021-10-12 16:23:41 +0300

    update weights

--- color2embed/config.py
+++ color2embed/config.py
@@ -1,6 +1,3 @@
-NORMALIZE_MEDIAN_BGR = [0.485, 0.456, 0.406]
-NORMALIZE_STD_BGR = [0.229, 0.224, 0.225]
-
 IMAGE_SIZE = 256
 
 # Path to ImageNet list, list is one path to image per line, sample:
@@ -8,12 +5,12 @@ IMAGE_SIZE = 256
 # n09428293/n09428293_35035.JPEG
 # n09428293/n09428293_46724.JPEG
 IMAGENET_LIST = '../imagenet.list'
-# Prefix to imagenet folder, os.path.join(imagenet_prefix, imagenet_list[i]) must be path to image
 
+# Prefix to imagenet folder, os.path.join(imagenet_prefix, imagenet_list[i]) must be path to image
 IMAGENET_PREFIX = '../datasets/images/ILSVRC/Data/CLS-LOC/train'
 
-TRAIN_BATCH_SIZE_PER_GPU = 2
-TRAIN_LR = 0.0001
-TRAIN_EPOCHS = 20
+TRAIN_BATCH_SIZE_PER_GPU = 32
+TRAIN_LR = 0.001
+TRAIN_EPOCHS = 10
 
 COLOR_EMBEDDING_DIM = 512
